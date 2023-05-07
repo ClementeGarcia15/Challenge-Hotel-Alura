b0fdb656f1d7e5a2c9c97edccc8ebd30ea636840
Cgarcia15 <75176552+ClementeGarcia15@users.noreply.github.com>
2023-05-06 19:51:01 -0600

    Se añadio el proyecto

---
+++ src/hotel/intermediarioUser/InterUser.java
@@ -0,0 +1,19 @@
+package hotel.intermediarioUser;
+
+/*
+ * Esta calse se utiliza para guardar el usuario y contraseña a registrar
+ */
+
+public class InterUser {
+	private String user;
+	
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+}
+
